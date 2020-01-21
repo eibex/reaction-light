f7f90338e912fcb0594d0cc962cc566e88be3603
eibex <40539455+eibex@users.noreply.github.com>
2020-01-21 14:57:42 +0100

    Linux fixes

--- rlightfm.py
+++ rlightfm.py
@@ -2,7 +2,7 @@ from random import randint
 from os import path
 import csv
 
-folder = "{}\\files".format(path.dirname(path.realpath(__file__)))
+folder = "{}/files".format(path.dirname(path.realpath(__file__)))
 wizard = {}
 wizardcache = {}
 cache = {}
@@ -10,7 +10,7 @@ cache = {}
 
 def readcache():
     try:
-        with open("{}\\cache.csv".format(folder), "r") as f:
+        with open("{}/cache.csv".format(folder), "r") as f:
             r = csv.reader(f, delimiter=",")
             for row in r:
                 cache[row[0]] = row[1]
@@ -35,13 +35,13 @@ def getch(r):
 
 
 def getcombo(r):
-    with open("{}\\".format(folder) + str(r) + ".csv", "r") as f:
+    with open("{}/".format(folder) + str(r) + ".csv", "r") as f:
         r = csv.reader(f, delimiter=",")
         return [i for i in r]
 
 
 def addids(message_id, r):
-    with open("{}\\cache.csv".format(folder), "a") as f:
+    with open("{}/cache.csv".format(folder), "a") as f:
         w = csv.writer(f, delimiter=",")
         w.writerow([message_id, r])
 
@@ -53,7 +53,7 @@ def getids(message_id):
 
 
 def getreactions(r):
-    with open("{}\\".format(folder) + r + ".csv", "r") as f:
+    with open("{}/".format(folder) + r + ".csv", "r") as f:
         r = csv.reader(f, delimiter=",")
         reactions = {}
         for row in r:
@@ -70,7 +70,7 @@ def listen(user, channel):
     ids = {}
 
     try:
-        with open("{}\\id.csv".format(folder), "r") as f:
+        with open("{}/id.csv".format(folder), "r") as f:
             read = csv.reader(f, delimiter=",")
             for i in read:
                 ids[i[0]] = i[1:]
@@ -80,7 +80,7 @@ def listen(user, channel):
         print("Creating id.csv")
 
     ids[r] = [str(user), str(channel)]
-    with open("{}\\id.csv".format(folder), "w") as f:
+    with open("{}/id.csv".format(folder), "w") as f:
         w = csv.writer(f, delimiter=",")
         for i in ids:
             row = [i, ids[i][0], ids[i][1]]
@@ -102,7 +102,7 @@ def step2(r, role, emoji, done=False):
     global wizardcache
     if done:
         wizard[r][3] += 1  # Set step3 (was 2)
-        with open("{}\\".format(folder) + str(r) + ".csv", "a") as f:
+        with open("{}/".format(folder) + str(r) + ".csv", "a") as f:
             w = csv.writer(f, delimiter=",")
             for i in wizardcache[r]:
                 w.writerow(i)
