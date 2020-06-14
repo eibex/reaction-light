4dd64e44a9f4e06459308b0381ae3c01714942af
Eibe <git@eibe.dev>
2020-06-14 18:14:57 +0200

    Move database into a class

--- core/database.py
+++ core/database.py
@@ -23,32 +23,27 @@ SOFTWARE.
 """
 
 
-from os import path
 import sqlite3
 from random import randint
 
 
-directory = path.dirname(path.dirname(path.realpath(__file__)))
-conn = sqlite3.connect(f"{directory}/files/reactionlight.db")
-
-cursor = conn.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
-    " 'reactionrole_id' INT);"
-)
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
-    " NVCARCHAR, 'role_id' INT);"
-)
-cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
-conn.commit()
-cursor.close()
-
-reactionrole_creation = {}
-
-
 class ReactionRoleCreationTracker:
-    def __init__(self, user, channel):
+    def __init__(self, user, channel, database):
+        self.conn = sqlite3.connect(database)
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
+            " 'reactionrole_id' INT);"
+        )
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
+            " NVCARCHAR, 'role_id' INT);"
+        )
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
+        self.conn.commit()
+        self.cursor.close()
+
         self.user = user
         self.channel = channel
         self.step = 0
@@ -58,206 +53,217 @@ class ReactionRoleCreationTracker:
         self._generate_reactionrole_id()
 
     def _generate_reactionrole_id(self):
-        c = conn.cursor()
         while True:
             self.reactionrole_id = randint(0, 100000)
-            c.execute(
+            self.cursor.execute(
                 "SELECT * FROM messages WHERE reactionrole_id = ?",
                 (self.reactionrole_id,),
             )
-            already_exists = c.fetchall()
+            already_exists = self.cursor.fetchall()
             if already_exists:
                 continue
-            c.close()
+            self.cursor.close()
             break
 
     def commit(self):
-        c = conn.cursor()
-        c.execute(
+        self.cursor.execute(
             "INSERT INTO 'messages' ('message_id', 'channel', 'reactionrole_id')"
             " values(?, ?, ?);",
             (self.message_id, self.target_channel, self.reactionrole_id),
         )
         for reaction in self.combos:
             role_id = self.combos[reaction]
-            c.execute(
+            self.cursor.execute(
                 "INSERT INTO 'reactionroles' ('reactionrole_id', 'reaction', 'role_id')"
                 " values(?, ?, ?);",
                 (self.reactionrole_id, reaction, role_id),
             )
-        conn.commit()
-        c.close()
+        self.conn.commit()
+        self.cursor.close()
 
 
-def start_creation(user, channel):
-    tracker = ReactionRoleCreationTracker(user, channel)
-    if not f"{user}_{channel}" in reactionrole_creation:
-        reactionrole_creation[f"{user}_{channel}"] = tracker
-        return True
-    return False
+class Database:
+    def __init__(self, database):
+        self.database = database
+        self.conn = sqlite3.connect(self.database)
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
+            " 'reactionrole_id' INT);"
+        )
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
+            " NVCARCHAR, 'role_id' INT);"
+        )
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
+        self.conn.commit()
+        self.cursor.close()
+
+        self.reactionrole_creation = {}
 
 
-def abort(user, channel):
-    if f"{user}_{channel}" in reactionrole_creation:
-        del reactionrole_creation[f"{user}_{channel}"]
-        return True
-    return False
+    def start_creation(self, user, channel):
+        tracker = ReactionRoleCreationTracker(user, channel, self.database)
+        if not f"{user}_{channel}" in self.reactionrole_creation:
+            self.reactionrole_creation[f"{user}_{channel}"] = tracker
+            return True
+        return False
 
 
-def step(user, channel):
-    if f"{user}_{channel}" in reactionrole_creation:
-        tracker = reactionrole_creation[f"{user}_{channel}"]
-        return tracker.step
-    return None
+    def abort(self, user, channel):
+        if f"{user}_{channel}" in self.reactionrole_creation:
+            del self.reactionrole_creation[f"{user}_{channel}"]
+            return True
+        return False
 
 
-def get_targetchannel(user, channel):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    return tracker.target_channel
+    def step(self, user, channel):
+        if f"{user}_{channel}" in self.reactionrole_creation:
+            tracker = self.reactionrole_creation[f"{user}_{channel}"]
+            return tracker.step
+        return None
 
 
-def get_combos(user, channel):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    return tracker.combos
+    def get_targetchannel(self, user, channel):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
+        return tracker.target_channel
 
 
-def step0(user, channel):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    tracker.step += 1
+    def get_combos(self, user, channel):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
+        return tracker.combos
 
 
-def step1(user, channel, target_channel):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    tracker.target_channel = target_channel
-    tracker.step += 1
-
-
-def step2(user, channel, role=None, reaction=None, done=False):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    if not done:
-        tracker.combos[reaction] = role
-    else:
+    def step0(self, user, channel):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
         tracker.step += 1
 
 
-def end_creation(user, channel, message_id):
-    tracker = reactionrole_creation[f"{user}_{channel}"]
-    tracker.message_id = message_id
-    try:
-        tracker.commit()
-    except sqlite3.Error as e:
-        return e
-    del reactionrole_creation[f"{user}_{channel}"]
+    def step1(self, user, channel, target_channel):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
+        tracker.target_channel = target_channel
+        tracker.step += 1
 
 
-def exists(message_id):
-    try:
-        c = conn.cursor()
-        c.execute("SELECT * FROM messages WHERE message_id = ?;", (message_id,))
-        result = c.fetchall()
-        c.close()
-        return result
-    except sqlite3.Error as e:
-        return e
+    def step2(self, user, channel, role=None, reaction=None, done=False):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
+        if not done:
+            tracker.combos[reaction] = role
+        else:
+            tracker.step += 1
 
 
-def get_reactions(message_id):
-    try:
-        c = conn.cursor()
-        c.execute(
-            "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
-        )
-        reactionrole_id = c.fetchall()[0][0]
-        c.execute(
-            "SELECT reaction, role_id FROM reactionroles WHERE reactionrole_id = ?;",
-            (reactionrole_id,),
-        )
-        combos = {}
-        for row in c:
-            reaction = row[0]
-            role_id = row[1]
-            combos[reaction] = role_id
-        c.close()
-        return combos
-    except sqlite3.Error as e:
-        return e
+    def end_creation(self, user, channel, message_id):
+        tracker = self.reactionrole_creation[f"{user}_{channel}"]
+        tracker.message_id = message_id
+        try:
+            tracker.commit()
+        except sqlite3.Error as e:
+            return e
+        del self.reactionrole_creation[f"{user}_{channel}"]
 
 
-def fetch_messages(channel):
-    try:
-        c = conn.cursor()
-        c.execute("SELECT message_id FROM messages WHERE channel = ?;", (channel,))
-        all_messages_in_channel = []
-        for row in c:
-            message_id = int(row[0])
-            all_messages_in_channel.append(message_id)
-        c.close()
-        return all_messages_in_channel
-    except sqlite3.Error as e:
-        return e
+    def exists(self, message_id):
+        try:
+            self.cursor.execute("SELECT * FROM messages WHERE message_id = ?;", (message_id,))
+            result = self.cursor.fetchall()
+            self.cursor.close()
+            return result
+        except sqlite3.Error as e:
+            return e
 
 
-def fetch_all_messages():
-    try:
-        c = conn.cursor()
-        c.execute(f"SELECT message_id, channel FROM messages;")
-        all_messages = {}
-        for row in c:
-            message_id = int(row[0])
-            channel_id = int(row[1])
-            all_messages[message_id] = channel_id
-        c.close()
-        return all_messages
-    except sqlite3.Error as e:
-        return e
+    def get_reactions(self, message_id):
+        try:
+            self.cursor.execute(
+                "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
+            )
+            reactionrole_id = self.cursor.fetchall()[0][0]
+            self.cursor.execute(
+                "SELECT reaction, role_id FROM reactionroles WHERE reactionrole_id = ?;",
+                (reactionrole_id,),
+            )
+            combos = {}
+            for row in self.cursor:
+                reaction = row[0]
+                role_id = row[1]
+                combos[reaction] = role_id
+            self.cursor.close()
+            return combos
+        except sqlite3.Error as e:
+            return e
 
 
-def delete(message_id):
-    try:
-        c = conn.cursor()
-        c.execute(
-            "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
-        )
-        reactionrole_id = c.fetchall()[0][0]
-        c.execute("DELETE FROM messages WHERE reactionrole_id = ?;", (reactionrole_id,))
-        c.execute(
-            "DELETE FROM reactionroles WHERE reactionrole_id = ?;", (reactionrole_id,)
-        )
-        conn.commit()
-        c.close()
-    except sqlite3.Error as e:
-        return e
+    def fetch_messages(self, channel):
+        try:
+            self.cursor.execute("SELECT message_id FROM messages WHERE channel = ?;", (channel,))
+            all_messages_in_channel = []
+            for row in self.cursor:
+                message_id = int(row[0])
+                all_messages_in_channel.append(message_id)
+            self.cursor.close()
+            return all_messages_in_channel
+        except sqlite3.Error as e:
+            return e
 
 
-def add_admin(role):
-    try:
-        c = conn.cursor()
-        c.execute("INSERT INTO 'admins' ('role_id') values(?);", (role,))
-        conn.commit()
-        c.close()
-    except sqlite3.Error as e:
-        return e
+    def fetch_all_messages(self):
+        try:
+            self.cursor.execute(f"SELECT message_id, channel FROM messages;")
+            all_messages = {}
+            for row in self.cursor:
+                message_id = int(row[0])
+                channel_id = int(row[1])
+                all_messages[message_id] = channel_id
+            self.cursor.close()
+            return all_messages
+        except sqlite3.Error as e:
+            return e
 
 
-def remove_admin(role):
-    try:
-        c = conn.cursor()
-        c.execute("DELETE FROM admins WHERE role_id = ?;", (role,))
-        conn.commit()
-        c.close()
-    except sqlite3.Error as e:
-        return e
+    def delete(self, message_id):
+        try:
+            self.cursor.execute(
+                "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
+            )
+            reactionrole_id = self.cursor.fetchall()[0][0]
+            self.cursor.execute("DELETE FROM messages WHERE reactionrole_id = ?;", (reactionrole_id,))
+            self.cursor.execute(
+                "DELETE FROM reactionroles WHERE reactionrole_id = ?;", (reactionrole_id,)
+            )
+            self.conn.commit()
+            self.cursor.close()
+        except sqlite3.Error as e:
+            return e
 
 
-def get_admins():
-    try:
-        c = conn.cursor()
-        c.execute("SELECT * FROM admins;")
-        admins = []
-        for row in c:
-            role_id = row[0]
-            admins.append(role_id)
-        c.close()
-        return admins
-    except sqlite3.Error as e:
-        return e
+    def add_admin(self, role):
+        try:
+            self.cursor.execute("INSERT INTO 'admins' ('role_id') values(?);", (role,))
+            self.conn.commit()
+            self.cursor.close()
+        except sqlite3.Error as e:
+            return e
+
+
+    def remove_admin(self, role):
+        try:
+            self.cursor.execute("DELETE FROM admins WHERE role_id = ?;", (role,))
+            self.conn.commit()
+            self.cursor.close()
+        except sqlite3.Error as e:
+            return e
+
+
+    def get_admins(self):
+        try:
+            self.cursor.execute("SELECT * FROM admins;")
+            admins = []
+            for row in self.cursor:
+                role_id = row[0]
+                admins.append(role_id)
+            self.cursor.close()
+            return admins
+        except sqlite3.Error as e:
+            return e
