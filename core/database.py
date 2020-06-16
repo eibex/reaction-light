426e5fcc4154673caade3db92a66c4696ac53761
Eibe <git@eibe.dev>
2020-06-16 19:17:45 +0200

    Close connection when closing cursor & general cleanup

--- core/database.py
+++ core/database.py
@@ -27,22 +27,27 @@ import sqlite3
 from random import randint
 
 
+def initialize(database):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
+        " 'reactionrole_id' INT);"
+    )
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
+        " NVCARCHAR, 'role_id' INT);"
+    )
+    cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
 class ReactionRoleCreationTracker:
     def __init__(self, user, channel, database):
-        self.conn = sqlite3.connect(database)
-        self.cursor = self.conn.cursor()
-
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
-            " 'reactionrole_id' INT);"
-        )
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
-            " NVCARCHAR, 'role_id' INT);"
-        )
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
-        self.conn.commit()
-        self.cursor.close()
+        self.database = database
+        initialize(self.database)
 
         self.user = user
         self.channel = channel
@@ -53,57 +58,47 @@ class ReactionRoleCreationTracker:
         self._generate_reactionrole_id()
 
     def _generate_reactionrole_id(self):
+        conn = sqlite3.connect(self.database)
         while True:
             self.reactionrole_id = randint(0, 100000)
-            self.cursor = self.conn.cursor()
-            self.cursor.execute(
+            cursor = conn.cursor()
+            cursor.execute(
                 "SELECT * FROM messages WHERE reactionrole_id = ?",
                 (self.reactionrole_id,),
             )
-            already_exists = self.cursor.fetchall()
+            already_exists = cursor.fetchall()
             if already_exists:
                 continue
-            self.cursor.close()
+            cursor.close()
+            conn.close()
             break
 
     def commit(self):
-        self.cursor = self.conn.cursor()
-        self.cursor.execute(
+        conn = sqlite3.connect(self.database)
+        cursor = conn.cursor()
+        cursor.execute(
             "INSERT INTO 'messages' ('message_id', 'channel', 'reactionrole_id')"
             " values(?, ?, ?);",
             (self.message_id, self.target_channel, self.reactionrole_id),
         )
         for reaction in self.combos:
             role_id = self.combos[reaction]
-            self.cursor.execute(
+            cursor.execute(
                 "INSERT INTO 'reactionroles' ('reactionrole_id', 'reaction', 'role_id')"
                 " values(?, ?, ?);",
                 (self.reactionrole_id, reaction, role_id),
             )
-        self.conn.commit()
-        self.cursor.close()
+        conn.commit()
+        cursor.close()
+        conn.close()
 
 
 class Database:
     def __init__(self, database):
         self.database = database
-        self.conn = sqlite3.connect(self.database)
-        self.cursor = self.conn.cursor()
-
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS 'messages' ('message_id' INT, 'channel' INT,"
-            " 'reactionrole_id' INT);"
-        )
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS 'reactionroles' ('reactionrole_id' INT, 'reaction'"
-            " NVCARCHAR, 'role_id' INT);"
-        )
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS 'admins' ('role_id' INT);")
-        self.conn.commit()
-        self.cursor.close()
+        initialize(self.database)
 
         self.reactionrole_creation = {}
-
 
     def start_creation(self, user, channel):
         tracker = ReactionRoleCreationTracker(user, channel, self.database)
@@ -168,10 +163,12 @@ class Database:
 
     def exists(self, message_id):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute("SELECT * FROM messages WHERE message_id = ?;", (message_id,))
-            result = self.cursor.fetchall()
-            self.cursor.close()
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM messages WHERE message_id = ?;", (message_id,))
+            result = cursor.fetchall()
+            cursor.close()
+            conn.close()
             return result
         except sqlite3.Error as e:
             return e
@@ -179,21 +176,23 @@ class Database:
 
     def get_reactions(self, message_id):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute(
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute(
                 "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
             )
-            reactionrole_id = self.cursor.fetchall()[0][0]
-            self.cursor.execute(
+            reactionrole_id = cursor.fetchall()[0][0]
+            cursor.execute(
                 "SELECT reaction, role_id FROM reactionroles WHERE reactionrole_id = ?;",
                 (reactionrole_id,),
             )
             combos = {}
-            for row in self.cursor:
+            for row in cursor:
                 reaction = row[0]
                 role_id = row[1]
                 combos[reaction] = role_id
-            self.cursor.close()
+            cursor.close()
+            conn.close()
             return combos
         except sqlite3.Error as e:
             return e
@@ -201,13 +200,15 @@ class Database:
 
     def fetch_messages(self, channel):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute("SELECT message_id FROM messages WHERE channel = ?;", (channel,))
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT message_id FROM messages WHERE channel = ?;", (channel,))
             all_messages_in_channel = []
-            for row in self.cursor:
+            for row in cursor:
                 message_id = int(row[0])
                 all_messages_in_channel.append(message_id)
-            self.cursor.close()
+            cursor.close()
+            conn.close()
             return all_messages_in_channel
         except sqlite3.Error as e:
             return e
@@ -215,14 +216,16 @@ class Database:
 
     def fetch_all_messages(self):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute(f"SELECT message_id, channel FROM messages;")
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT message_id, channel FROM messages;")
             all_messages = {}
-            for row in self.cursor:
+            for row in cursor:
                 message_id = int(row[0])
                 channel_id = int(row[1])
                 all_messages[message_id] = channel_id
-            self.cursor.close()
+            cursor.close()
+            conn.close()
             return all_messages
         except sqlite3.Error as e:
             return e
@@ -230,50 +233,58 @@ class Database:
 
     def delete(self, message_id):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute(
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute(
                 "SELECT reactionrole_id FROM messages WHERE message_id = ?;", (message_id,)
             )
-            reactionrole_id = self.cursor.fetchall()[0][0]
-            self.cursor.execute("DELETE FROM messages WHERE reactionrole_id = ?;", (reactionrole_id,))
-            self.cursor.execute(
+            reactionrole_id = cursor.fetchall()[0][0]
+            cursor.execute("DELETE FROM messages WHERE reactionrole_id = ?;", (reactionrole_id,))
+            cursor.execute(
                 "DELETE FROM reactionroles WHERE reactionrole_id = ?;", (reactionrole_id,)
             )
-            self.conn.commit()
-            self.cursor.close()
+            conn.commit()
+            cursor.close()
+            conn.close()
         except sqlite3.Error as e:
             return e
 
 
     def add_admin(self, role):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute("INSERT INTO 'admins' ('role_id') values(?);", (role,))
-            self.conn.commit()
-            self.cursor.close()
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO 'admins' ('role_id') values(?);", (role,))
+            conn.commit()
+            cursor.close()
+            conn.close()
         except sqlite3.Error as e:
             return e
 
 
     def remove_admin(self, role):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute("DELETE FROM admins WHERE role_id = ?;", (role,))
-            self.conn.commit()
-            self.cursor.close()
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM admins WHERE role_id = ?;", (role,))
+            conn.commit()
+            cursor.close()
+            conn.close()
         except sqlite3.Error as e:
             return e
 
 
     def get_admins(self):
         try:
-            self.cursor = self.conn.cursor()
-            self.cursor.execute("SELECT * FROM admins;")
+            conn = sqlite3.connect(self.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM admins;")
             admins = []
-            for row in self.cursor:
+            for row in cursor:
                 role_id = row[0]
                 admins.append(role_id)
-            self.cursor.close()
+            cursor.close()
+            conn.close()
             return admins
         except sqlite3.Error as e:
             return e
